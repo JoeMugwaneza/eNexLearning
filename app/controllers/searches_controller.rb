9a0d1305d0a1340d83552b83359a53c5233e241d
Jean Luc Abayo <luc.bayo@gmail.com>
2017-10-02 21:43:26 +0200

    added text editor gem and search box

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+  
+  def index
+     
+  end
+  def search
+   @results = Subject.search(params[:query]) + Course.search(params[:query]) + Chapter.search(params[:query]) +Section.search(params[:query])
+   render :index
+  end
+
+end
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,4 +3,8 @@ class Chapter < ApplicationRecord
   has_many :sections
   validates_presence_of :title, :message => "Field required"
   validates_presence_of :body, :message => "Field required"
+  def self.search(query)
+    query = query.downcase
+    where("lower(title) like ? OR lower(body) like? OR lower(tags) like ?", "%#{query}%", "%#{query}%", "%#{query}%")
+  end
 end
