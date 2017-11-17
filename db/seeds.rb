940ad7fd9ac3c2da13c582b0d1e8703564d25535
Jean Luc Abayo <luc.bayo@gmail.com>
2017-11-18 00:54:35 +0200

    added some data in seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-20.times do
-	User.create(
-		first_name: Faker::Name.first_name,
-		last_name: Faker::Name.last_name, 
-		email: Faker::Internet.unique.email,
-		tel: Faker::PhoneNumber.cell_phone,
-		country: Faker::Address.country,
-		district: Faker::Address.city,
-		role: "student",
-		password: "password",
-		user_profile: "http://res.cloudinary.com/nexpals/image/upload/v1510933584/default-user_uzaoin.png"
-	)
-end
+# require 'faker'
+# 20.times do
+# 	User.create(
+# 		first_name: Faker::Name.first_name,
+# 		last_name: Faker::Name.last_name, 
+# 		email: Faker::Internet.unique.email,
+# 		tel: Faker::PhoneNumber.cell_phone,
+# 		country: Faker::Address.country,
+# 		district: Faker::Address.city,
+# 		role: "student",
+# 		password: "password",
+# 		user_profile: "http://res.cloudinary.com/nexpals/image/upload/v1510933584/default-user_uzaoin.png"
+# 	)
+# end
 
 3.times do
 	Subject.create(
-		title: "Computer Programing",
+		title: "Mathamatics",
 		description: "Learn how to program drawings, animations, and games using JavaScript & ProcessingJS, or learn how to create webpages with HTML & CSS. You can share whatever you create, explore what others have created and learn from each other!",
 		image: "http://www.esolution24.com/wp-content/uploads/2017/05/computer.jpg"
 	)
